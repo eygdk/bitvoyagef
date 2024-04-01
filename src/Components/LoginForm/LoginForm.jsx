d2fd892ca3f7b5ad234204834c2a586d87858bcd
jongwonOh <ohwj97@naver.com>
2024-04-01 16:05:45 +0900

    로그인 화면 레이아웃

---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './LoginForm.css';
+import { FaUser, FaLock } from "react-icons/fa";
+
+const LoginForm = () => {
+    return (
+        <div className='wrapper'>
+            <form action="">
+                <h1>로그인 연동 화면</h1>
+                <div className="input-box">
+                    <input type="text" placeholder='아이디' required />
+                    <FaUser className='icon' />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder='비밀번호' required />
+                    <FaLock className='icon' />
+                </div>
+                    <div className="remember-forgot">
+                        <label><input type="checkbox" />기억하기</label>
+                        <a href="#">비밀번호찾기</a>
+                    </div>
+
+                    <button type="submit">Login</button>
+
+                    <div className="register-link">
+                        <p>계정이 없으십니까? <a href="#">회원가입</a></p>
+                    </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
